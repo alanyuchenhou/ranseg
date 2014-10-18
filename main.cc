cae7971374fe7809fd7072382ea7723eba9b3d10
armensly <a.abnousi@gmail.com>
2014-10-17 17:12:20 -0700

    Update main.cc
    parallel_prefix function is added. I tested it for parallel sum and it works, but we should add the code for matrix multiplication and then minor changes might be required for this function also!

--- main.cc
+++ main.cc
@@ -18,6 +18,31 @@ using std::uint8_t;
 using std::uint16_t;
 using std::uint32_t;
 
+//output, and parameter "out" should be changed to int*; 
+//the line commented out was for parallel sum testing it should be replaced by matrix mult!
+//output should be possibly changed to another thing! I'm not sure yet!
+int parallelPrefix(int out, int rank, int process_count)
+{
+	int in;
+	int mate=rank;
+	for (int i=1; i<process_count;i=i*2)
+	{
+		if(rank%(2*i) < (i))
+		{
+			mate=rank+i;
+		}
+		else
+		{
+			mate=rank-i;
+		}
+		cout<<"rank="<<rank<<" i="<<i<<" mate="<<mate<<'\n';
+		MPI_Status status;
+		MPI_Sendrecv(&out,4,MPI_INT,mate,0,&in,4,MPI_INT,mate,0,MPI_COMM_WORLD,&status);
+		//out+=in;
+	}
+	return out;
+}
+
 long getTime (struct timeval time_begin, struct timeval time_end) {
   return (time_end.tv_sec*1000000 + time_end.tv_usec) -
     (time_begin.tv_sec*1000000 + time_begin.tv_usec);
